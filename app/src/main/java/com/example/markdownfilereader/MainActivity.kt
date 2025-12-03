c78e413756b550ecb6f84dfe7ff85bad205716fb
Tyree McGee <ut10rocks@gmail.com>
2025-12-02 20:06:23 -0600

    Copy to clipboard support

--- app/src/main/java/com/example/markdownfilereader/MainActivity.kt
+++ app/src/main/java/com/example/markdownfilereader/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -94,8 +95,10 @@ fun MarkdownScreen(
                     .padding(top = 48.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                RichText {
-                    Markdown(content = markdownContent)
+                SelectionContainer {
+                    RichText {
+                        Markdown(content = markdownContent)
+                    }
                 }
             }
 
