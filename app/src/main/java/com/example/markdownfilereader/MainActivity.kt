d0a81cafe4cbd407af536230430c3003d6c85c2f
Tyree McGee <ut10rocks@gmail.com>
2025-12-02 20:30:09 -0600

    Stays open during screen rotation

--- app/src/main/java/com/example/markdownfilereader/MainActivity.kt
+++ app/src/main/java/com/example/markdownfilereader/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,7 +51,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MarkdownRenderer(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    var markdownContent by remember { mutableStateOf<String?>(null) }
+    var markdownContent by rememberSaveable { mutableStateOf<String?>(null) }
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
